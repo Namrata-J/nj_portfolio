0700dca10674844826914bb6d4749d14282d25bd
namrata <namrata.jain@pensionbox.in>
2026-02-23 13:20:53 +0530

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let repo = '';
+if (isGithubActions) {
+  // Extract repository name from GITHUB_REPOSITORY (e.g., Namrata-J/nj_portfolio)
+  const repoName = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  repo = `/${repoName}`;
+}
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: repo,
+  assetPrefix: repo,
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
